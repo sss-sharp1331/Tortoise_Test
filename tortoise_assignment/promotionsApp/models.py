4987f0be1796721c58f8d68c0fca72eb70a27d14
sss-sharp1331 <sonalsharma.iitkgp@gmail.com>
2022-06-27 03:26:33 +0530

    final-changes

--- tortoise_assignment/promotionsApp/models.py
+++ tortoise_assignment/promotionsApp/models.py
@@ -2,32 +2,23 @@ from email.policy import default
 from random import choices, expovariate
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
-#from .benifit_types import *
-
-
-VOUCHER = "VOUCHER"
-CASHBACK = "CASHBACK"
-default = CASHBACK
-benifit_types = [
-    (VOUCHER, "Voucher"),
-    (CASHBACK, "Cashback")
-]
-
+from .benifit_types import *
 
 # Promotion Data Model
+
+
 class PlanInfo(models.Model):
     plan_id = models.AutoField(primary_key=True)
-    # planID = models.TextField(null=False)
     planName = models.TextField(null=False)
     amountOptions = models.IntegerField()
     tenureOptions = models.TextField()
     benefitPercentage = models.IntegerField()
     benefitType = models.CharField(
         choices=benifit_types, default=VOUCHER, max_length=256)
-    # startedDate = models.DateTimeField(null=False)
+    startedDate = models.DateTimeField(null=True),
+    usersNumber = models.IntegerField(null=True),
+    timePeriod = models.IntegerField(null=True)
 
-#    def __unicode__(self):
-#        return '%s' % self.planID
 
 # User Data Model
 
@@ -35,10 +26,9 @@ class PlanInfo(models.Model):
 class UserData(models.Model):
     user_id = models.AutoField(primary_key=True)
     planID = models.ForeignKey(PlanInfo, on_delete=models.CASCADE)
-    # userID = models.TextField(null=False)
     selectedAmount = models.IntegerField(null=False)
     selectedTenure = models.TextField(null=False)
-    # startedDate = models.DateTimeField(null=False)
+    startedDate = models.DateTimeField(null=True)
     depositedAmount = models.IntegerField(null=False)
 
 
